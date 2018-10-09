974cc1e9d1bc6777c44aebf633d669f96ecbd835
hellikopter <hellikopter@users.noreply.github.com>
2018-10-09 09:18:19 +0100

    update schema calling funcion with diff method

--- GraphDBSchemaUpdate.ps1
+++ GraphDBSchemaUpdate.ps1
@@ -18,7 +18,9 @@ This script is for use as a part of deployment in VSTS only.
 Param(
     [Parameter(Mandatory=$true)] [string] $APIResourceGroupName,
 	[Parameter(Mandatory=$true)] [string] $APIManagementName,
-    [Parameter(Mandatory=$true)] [string] $OntologyFileLocation,	
+    [Parameter(Mandatory=$true)] [string] $OntologyFileLocation,
+	[Parameter(Mandatory=$true)] [string] $AzureFunctionsName,
+	[Parameter(Mandatory=$true)] [string] $OrchestrationResourceGroupName,
 	[Parameter(Mandatory=$true)] [string] $APIPrefix	
 )
 
@@ -28,6 +30,29 @@ function Log([Parameter(Mandatory=$true)][string]$LogText){
     Write-Host ("{0} - {1}" -f (Get-Date -Format "HH:mm:ss.fff"), $LogText)
 }
 
+function Get-JMXAttribute([Parameter(Mandatory=$true)][string]$AttributeName){
+    Log "Gets $AttributeName on master"
+    $bodyTxt=@{
+        "type"="read";
+        "mbean"= "ReplicationCluster:name=ClusterInfo/Master";
+        "attribute"= "$AttributeName";
+    }
+    $bodyJson=ConvertTo-Json $bodyTxt
+    $response=Invoke-RestMethod -Uri "https://$APIManagementName.azure-api.net/jmx" -Method Post -ContentType "application/json" -Body $bodyJson -Headers $header -TimeoutSec 15
+    $response.value
+}
+
+Log "Retrive trigger code for $AzureFunctionsName"
+$properties=Invoke-AzureRmResourceAction -ResourceGroupName $OrchestrationResourceGroupName -ResourceType Microsoft.Web/sites/config -ResourceName "$AzureFunctionsName/publishingcredentials" -Action list -ApiVersion 2015-08-01 -Force
+$base64Info=[Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $properties.properties.publishingUserName,$properties.properties.publishingPassword)))
+$masterKeyResponse=Invoke-RestMethod -Uri "https://$AzureFunctionsName.scm.azurewebsites.net/api/functions/admin/masterkey" -Headers @{Authorization=("Basic {0}" -f $base64Info)} -Method GET
+
+Log "Read file in utf-8"
+$txt=Get-Content $OntologyFileLocation -Encoding UTF8
+
+Log "Update schema"
+Invoke-RestMethod -Uri "https://$AzureFunctionsName.azurewebsites.net/api/GraphDBSchemaUpdate?code=$($masterKeyResponse.masterKey)" -Method Post -ContentType "text/turtle" -Body $txt -TimeoutSec 300 -Verbose
+
 Log "Get API Management context"
 $management=New-AzureRmApiManagementContext -ResourceGroupName $APIResourceGroupName -ServiceName $APIManagementName
 Log "Get product id"
@@ -36,13 +61,36 @@ Log "Retrives subscription"
 $subscription=Get-AzureRmApiManagementSubscription -Context $management -ProductId $apiReleaseProductId
 $subscriptionKey=$subscription.PrimaryKey
 
-$api="https://$APIManagementName.azure-api.net"
-$header=@{"Ocp-Apim-Subscription-Key"="$subscriptionKey"; "Api-Version"=$APIPrefix}
+$header=@{"Ocp-Apim-Subscription-Key"="$subscriptionKey";"Api-Version"="$APIPrefix"}
 
-Log "Read file in utf-8"
-$txt=Get-Content $OntologyFileLocation -Encoding UTF8
+Log "Wait 10sec"
+Start-Sleep -Seconds 10
 
-Log "Add ontology"
-Invoke-RestMethod -Uri "$api/rdf4j/repositories/Master/statements" -Method Post -Body $txt -ContentType "application/sparql-update" -TimeoutSec 180 -Headers $header -Verbose
+$result=1
+$counter=0;
+while($result -ne 0) {
+    Log "Counter $counter"
+    $status=Get-JMXAttribute -AttributeName "NodeStatus"
+    $status
+    $result=1
+    foreach($node in $status){
+        Log "Status $node"
+        $flag=$node.Split(' ')[0]
+        if ($flag -eq "[ON]") {
+            $result=0
+            break
+        }
+    }
+    if ($result -ne 0){
+        Log "Wait 10 seconds, response ($flag)"
+        $counter++
+        Start-Sleep -Seconds 10
+    }
+    if (($counter -gt 20) -and ($result -ne 0)) {
+        throw "Invalid status of cluster"
+    }
+}
+
+Get-JMXAttribute -AttributeName "NodeStatus"
 
 Log "Job done"
